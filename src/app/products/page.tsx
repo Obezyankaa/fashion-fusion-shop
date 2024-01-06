8293f137c49bd4c97e547ad8c542d72644400c6e
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-01-06 17:32:31 +0700

    task 19

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
 
 export default function ProductList() {
-    return (
-      <>
-        <h1>ProductList List</h1>
-        <div> product one</div>
-        <div> product two</div>
-        <div> product three</div>
-        <Link href="/">вернуться на главную</Link>
-      </>
-    );
+
+  const productId = 56
+
+  return (
+    <>
+      <h1>ProductList List</h1>
+      <div>
+        <Link href="/products/1">product one</Link>
+      </div>
+      <div>
+        <Link href="/products/2">product two</Link>
+      </div>
+      <div>
+        <Link href="/products/3" replace>product three</Link>
+      </div>
+      <div>
+        <Link href={`products/${productId}`}>product { productId}</Link>
+      </div>
+      <Link href="/">вернуться на главную</Link>
+    </>
+  );
 }
