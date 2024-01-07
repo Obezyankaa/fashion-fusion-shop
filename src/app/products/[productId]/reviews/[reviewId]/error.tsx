657dd76d2909836fa0501dd1c2c20b231ea92862
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-01-07 16:44:54 +0700

    task 25

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,16 @@
 "use client";
 
-export default function ErrorBoundary({error} : { error: Error }) {
-    return <h1>{error.message} </h1>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+    return (
+        <div>
+            <h1>{error.message} </h1>
+            <button onClick={reset}>Try again</button>
+        </div>
+    );
 }
