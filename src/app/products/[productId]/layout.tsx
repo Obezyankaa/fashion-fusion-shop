347894b295bb39e8e58d3447ee939a324d43b65d
Vladislav Voloshanovskiy <voloshanowky@yandex.ru>
2024-01-07 17:49:41 +0700

    task 28

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,17 +1,17 @@
-function getRandomInt(count: number) {
-  return Math.floor(Math.random() * count);
-}
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
 
 export default function ProductDetailsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const random = getRandomInt(2);
+  // const random = getRandomInt(2);
 
-  if (random === 1) {
-    throw new Error("Error loading productId");
-  }
+  // if (random === 1) {
+  //   throw new Error("Error loading productId");
+  // }
   return (
     <>
       {children}
